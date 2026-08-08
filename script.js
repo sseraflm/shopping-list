64393db0a030080fb036cf0247ce5a47f1b479f8
sseraflm <anomyuslol@gmail.com>
2026-08-08 22:07:09 +0200

    feat: add product creation, deletion, and bought status

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 let productCount = document.getElementById("productCount");
 let boughtProductCount = document.getElementById("boughtProductCount");
 let productList = document.getElementById("productList");
-let boughtProductList = document.getElementById("boughtProductList");
-
+let createProductButton = document.getElementById("createProduct");
+let noProducts = document.getElementById("noProducts");
 let products = [
     {
+        id: 1,
         name: "Ado's CD",
         quantity: 43,
         category: "Music",
         isBought: true,
     },
     {
+        id: 2,
         name: "Mazda RX-7",
         quantity: 1,
         category: "Vehicles",
         isBought: false,
     },
     {
+        id: 3,
         name: "RTX 5090",
         quantity: 1,
         category: "Computer parts",
         isBought: false,
     },
     {
+        id: 4,
         name: "Water",
         quantity: 1,
         category: "Drinks",
@@ -33,6 +37,7 @@ let products = [
 function createProductItem(product) {
     let productCard = document.createElement("div");
     productCard.className = "product-card";
+    productCard.dataset.id = product.id;
 
     let productName = document.createElement("h2");
     productName.innerText = `Name: ${product.name}`;
@@ -44,22 +49,72 @@ function createProductItem(product) {
     productCategory.innerText = `Product category: ${product.category}`;
 
     let productIsBought = document.createElement("p");
-    if (product.isBought) {
+
+    let boughtCheckbox = document.createElement("input");
+    boughtCheckbox.setAttribute("type", "checkbox");
+    boughtCheckbox.classList.add("boughtCheck");
+    boughtCheckbox.checked = product.isBought;
+    if (boughtCheckbox.checked) {
         productIsBought.innerText = `Is the product bought: Yes`;
         productCard.classList.add("bought");
     } else {
         productIsBought.innerText = `Is the product bought: No`;
+        productCard.classList.remove("bought");
     }
-    productCard.append(productName, productQuantity, productCategory, productIsBought);
+
+    boughtCheckbox.addEventListener("change", () => {
+        product.isBought = boughtCheckbox.checked;
+        if (boughtCheckbox.checked) {
+            productIsBought.innerText = `Is the product bought: Yes`;
+            updateBoughtProductsCount(products, boughtProductCount);
+            productCard.classList.add("bought");
+        } else {
+            productIsBought.innerText = `Is the product bought: No`;
+            updateBoughtProductsCount(products, boughtProductCount);
+            productCard.classList.remove("bought");
+        }
+    });
+
+    let boughtCheckLabel = document.createElement("label");
+    boughtCheckLabel.innerText = "Check to mark the product as bought.";
+    let br = document.createElement("br");
+
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("removeObject");
+    deleteButton.innerText = "Delete Object";
+    deleteButton.addEventListener("click", deleteObject);
+
+    productCard.append(
+        productName,
+        productQuantity,
+        productCategory,
+        productIsBought,
+        boughtCheckbox,
+        boughtCheckLabel,
+        br,
+        deleteButton,
+    );
 
     return productCard;
 }
-
+function deleteObject(event) {
+    event.preventDefault();
+    let closestProduct = event.target.closest(".product-card");
+    let productId = closestProduct.dataset.id;
+    let numberProductId = Number(productId);
+    let productIndex = products.findIndex(product => product.id === numberProductId);
+    products.splice(productIndex, 1);
+    renderProducts(products, productList);
+    updateAllProductCount(products, productCount);
+    updateBoughtProductsCount(products, boughtProductCount);
+}
 function renderProducts(productsArray, container) {
     container.innerHTML = "";
     if (productsArray.length === 0) {
-        alert("The shopping list is empty.");
+        noProducts.classList.remove("hidden");
         return;
+    } else if (productsArray.length > 0) {
+        noProducts.classList.add("hidden");
     }
     for (let product of productsArray) {
         let productCard = createProductItem(product);
@@ -83,8 +138,31 @@ function updateBoughtProductsCount(productsArray, container) {
     container.innerText = `There are currently ${boughtProducts.length} products bought.`;
 }
 
+let objectId = 5;
+
+function createNewProducts(event) {
+    event.preventDefault();
+    let nameInput = document.getElementById("nameInput");
+    let quantityInput = document.getElementById("quantityInput");
+    let categoryInput = document.getElementById("categoryInput");
+
+    let newObject = {
+        id: objectId++,
+        name: nameInput.value,
+        quantity: quantityInput.value,
+        category: categoryInput.value,
+        isBought: false,
+    };
+    products.push(newObject);
+    nameInput.value = "";
+    quantityInput.value = "";
+    categoryInput.value = "";
+    renderProducts(products, productList);
+    updateAllProductCount(products, productCount);
+}
+
 renderProducts(products, productList);
 updateAllProductCount(products, productCount);
 updateBoughtProductsCount(products, boughtProductCount);
-let boughtProducts = getBoughtProducts(products);
-renderProducts(boughtProducts, boughtProductList);
+
+createProductButton.addEventListener("click", createNewProducts);
